0592648328fb4d43dcaa355043b87a45bdaa5f2b
Echo002 <xugaohero@163.com>
2020-02-05 17:49:06 -0800

    update

---
+++ dealdata.py
@@ -0,0 +1,272 @@
+# dealdata for BeeSwarm、Volcano、xyplot
+'''
+BeeSwarm Plot：
+    title: protein/gender
+    table head：control/50、tumor/50、cancer/50、control/60、tumor/60、cancer/60、control/70、tumor/70、cancer/70
+    x：catagory/age
+    y：protein
+XY Plot：
+    title：protein/catagory
+    table head：age、protein
+    x：age
+    y：protein
+Vocanlo Plot：
+    title：catagory/catagory
+    table head：FC、-log10(T-test)
+'''
+
+# Read *.xlsx file
+import os
+import numpy as np
+import pandas as pd
+from openpyxl import load_workbook
+from scipy import stats
+import math
+from scipy.stats import pearsonr
+
+try:
+    t = pd.DataFrame(pd.read_excel('demoDataSet1.xlsx'))  # header = 1 表示从第一行开始
+except FileNotFoundError:
+    print("File not exist！")
+    exit()
+
+# paramter set
+
+t_head = t.columns
+catagory = ['control', 'tumor', 'cancer']
+catagory1 = ['Control', 'Case']
+gender = [0, 1]
+gender_CN = ['男', '女']
+
+# class set
+
+
+class BeeSwarmPlot:
+    def __init__(self, t):
+        self.t = t
+        self.t_head = t.columns
+
+    def gen_protein_class(self, gender, count_protein):
+        '''
+        :param gender:gender class
+        :param count_protein: what kind of protein you want
+        :return: a list include control/tumor/cancer + 50/60/70
+        '''
+        r = []
+        # 50
+        r.append(list(t.loc[(t['gender'] == gender) & (t['age'] < 50) & (t['catagory'] == 'control'), t_head[count_protein]]))
+        r.append(list(t.loc[(t['gender'] == gender) & (t['age'] < 50) & (t['catagory'] == 'tumor'), t_head[count_protein]]))
+        r.append(list(t.loc[(t['gender'] == gender) & (t['age'] < 50) & (t['catagory'] == 'cancer'), t_head[count_protein]]))
+
+        # 60
+        r.append(list(t.loc[(t['gender'] == gender) & (t['age'] > 50) & (t['age'] < 70) & (t['catagory'] == 'control'), t_head[count_protein]]))
+        r.append(list(t.loc[(t['gender'] == gender) & (t['age'] > 50) & (t['age'] < 70) & (t['catagory'] == 'tumor'), t_head[count_protein]]))
+        r.append(list(t.loc[(t['gender'] == gender) & (t['age'] > 50) & (t['age'] < 70) & (t['catagory'] == 'cancer'), t_head[count_protein]]))
+
+        # 70
+        r.append(list(t.loc[(t['gender'] == gender) & (t['age'] > 70) & (t['catagory'] == 'control'), t_head[count_protein]]))
+        r.append(list(t.loc[(t['gender'] == gender) & (t['age'] > 70) & (t['catagory'] == 'tumor'), t_head[count_protein]]))
+        r.append(list(t.loc[(t['gender'] == gender) & (t['age'] > 70) & (t['catagory'] == 'cancer'), t_head[count_protein]]))
+        return r
+
+    def WriteSheet(self, ListTotal, ExcelPatch, SheetName):
+        '''
+        :param ListTotal: only list can received
+        :param ExcelPatch: excel path
+        :param SheetName: sheet name
+        :return: No return, output a excel file, if not, create it.
+        '''
+        if not os.path.exists(ExcelPatch):
+            NewFile = pd.DataFrame()
+            NewFile.to_excel(ExcelPatch)
+        data = pd.DataFrame(ListTotal)
+        data = data.transpose()
+        # Create a new sheet and write data
+        excelWriter = pd.ExcelWriter(ExcelPatch, engine='openpyxl')
+        book = load_workbook(excelWriter.path)
+        excelWriter.book = book
+        data.to_excel(excel_writer=excelWriter, sheet_name=SheetName, index=None)
+        excelWriter.close()
+
+
+class XYplot:
+    def __init__(self, t):
+        self.t = t
+
+    def gen_protein_class(self, count_protein, cata):
+        '''
+        :param count_protein: what kind of protein you want
+        :param cata: one of "control, tumor and cancer"
+        :return: a list include age + protein
+        '''
+        r = []
+        r.append(list(t.loc[(t['catagory'] == catagory[cata]), 'age']))
+        r.append(list(t.loc[(t['catagory'] == catagory[cata]), t_head[count_protein]]))
+        return r
+
+    def WriteSheet(self, ListTotal, ExcelPatch, SheetName):
+        '''
+        :param ListTotal: only list can received
+        :param ExcelPatch: excel path
+        :param SheetName: sheet name
+        :return: No return, output a excel file, if not, create it.
+        '''
+        if not os.path.exists(ExcelPatch):
+            NewFile = pd.DataFrame()
+            NewFile.to_excel(ExcelPatch)
+        data = pd.DataFrame(ListTotal)
+        data = data.transpose()
+        # Create a new sheet and write data
+        excelWriter = pd.ExcelWriter(ExcelPatch, engine='openpyxl')
+        book = load_workbook(excelWriter.path)
+        excelWriter.book = book
+        data.to_excel(excel_writer=excelWriter, sheet_name=SheetName, index=None)
+        excelWriter.close()
+
+    def calcpearsonr(self, listA, listB):
+        arrayA = np.array(listA)
+        arrayB = np.array(listB)
+        return pearsonr(arrayA, arrayB)
+
+
+class Vocanloplot:
+    def __init__(self, t):
+        self.t = t
+
+    def calculate_param(self, gender, count_protein):
+        '''
+        :param gender:gender class
+        :return: a list include control/tumor/cancer + 50/60/70
+        '''
+        control_list = list(t.loc[(t['gender'] == gender) & (t['catagory'] == 'control'), t_head[count_protein]])
+        control_mediam = np.nanmedian(control_list)
+
+        tumor_list = list(t.loc[(t['gender'] == gender) & (t['catagory'] == 'tumor'), t_head[count_protein]])
+        tumor_mediam = np.nanmedian(tumor_list)
+
+        cancer_list = list(t.loc[(t['gender'] == gender) & (t['catagory'] == 'cancer'), t_head[count_protein]])
+        cancer_mediam = np.nanmedian(cancer_list)
+
+        FC_CT = control_mediam / tumor_mediam
+        FC_CC = control_mediam / cancer_mediam
+
+        T_test_CT = list(stats.ttest_ind(control_list, tumor_list, nan_policy='omit'))
+        T_test_CC = list(stats.ttest_ind(control_list, cancer_list, nan_policy='omit'))
+        T_test_CT[1] = - math.log(T_test_CT[1], 10)
+        T_test_CC[1] = - math.log(T_test_CC[1], 10)
+
+        return [FC_CT, FC_CC, T_test_CT[1], T_test_CC[1]]
+
+    def WriteSheet(self, ListTotal, ExcelPatch, SheetName):
+        '''
+        :param ListTotal: only list can received
+        :param ExcelPatch: excel path
+        :param SheetName: sheet name
+        :return: No return, output a excel file, if not, create it.
+        '''
+        if not os.path.exists(ExcelPatch):
+            NewFile = pd.DataFrame()
+            NewFile.to_excel(ExcelPatch)
+        data = pd.DataFrame(ListTotal)
+        data = data.transpose()
+        # Create a new sheet and write data
+        excelWriter = pd.ExcelWriter(ExcelPatch, engine='openpyxl')
+        book = load_workbook(excelWriter.path)
+        excelWriter.book = book
+        data.to_excel(excel_writer=excelWriter, sheet_name=SheetName, index=None)
+        excelWriter.close()
+
+# function set
+
+
+def calc_corr(a, b):
+    a_avg = sum(a) / len(a)
+    b_avg = sum(b) / len(b)
+    # 计算分子，协方差————按照协方差公式，本来要除以n的，由于在相关系数中上下同时约去了n，于是可以不除以n
+    cov_ab = sum([(x - a_avg) * (y - b_avg) for x, y in zip(a, b)])
+    # 计算分母，方差乘积————方差本来也要除以n，在相关系数中上下同时约去了n，于是可以不除以n
+    sq = math.sqrt(sum([(x - a_avg) ** 2 for x in a]) * sum([(x - b_avg) ** 2 for x in b]))
+    corr_factor = cov_ab / sq
+    return corr_factor
+
+
+def deleteSheet(ExcelName, SheetName):
+    try:
+        wb = load_workbook(ExcelName)
+        ws = wb[SheetName]
+        wb.remove(ws)
+        wb.save(ExcelName)
+    except KeyError:
+        print("sheet not exist！")
+        exit()
+
+
+def genBeeSwarm():
+    plot = BeeSwarmPlot(t)
+    for gender in range(2):
+        for count_protein in range(4, len(t_head)):
+            eData = plot.gen_protein_class(gender, count_protein)
+            plot.WriteSheet(eData, r'BeeSwarm.xlsx', str(gender) + ' ' + t_head[count_protein])
+    deleteSheet(r'BeeSwarm.xlsx', 'Sheet1')
+
+
+def genXYplot():
+    plot = XYplot(t)
+    catagory = ['control', 'tumor', 'cancer']
+    for count_protein in range(4, len(t_head)):
+        for class_cata in range(3):
+            eData = plot.gen_protein_class(count_protein, class_cata)
+            plot.WriteSheet(eData, r'XYplot.xlsx', t_head[count_protein] + ' ' + catagory[class_cata])
+    deleteSheet(r'XYplot.xlsx', 'Sheet1')
+
+
+def genVolcanoplot():
+    each_protein_list = []
+    plot = Vocanloplot(t)
+    for gender in range(2):
+        for count_protein in range(4, len(t_head)):
+            each_protein_list.append(plot.calculate_param(gender, count_protein))
+    xct_plot = []
+    yct_plot = []
+    xcc_plot = []
+    ycc_plot = []
+    for i in range(4, len(t_head)):
+        xct_plot.append(each_protein_list[i-4][0])
+        xcc_plot.append(each_protein_list[i-4][1])
+        yct_plot.append(each_protein_list[i-4][2])
+        ycc_plot.append(each_protein_list[i-4][3])
+    m = [xct_plot, yct_plot, xcc_plot, ycc_plot]
+    xct_plot = []
+    yct_plot = []
+    xcc_plot = []
+    ycc_plot = []
+    for i in range(int((len(t_head)-4)/2), len(t_head)):
+        xct_plot.append(each_protein_list[i-4][0])
+        xcc_plot.append(each_protein_list[i-4][1])
+        yct_plot.append(each_protein_list[i-4][2])
+        ycc_plot.append(each_protein_list[i-4][3])
+    fm = [xct_plot, yct_plot, xcc_plot, ycc_plot]
+    plot.WriteSheet(m, r'Volcanoplot.xlsx', 'male')
+    plot.WriteSheet(fm, r'Volcanoplot.xlsx', 'female')
+    deleteSheet(r'Volcanoplot.xlsx', 'Sheet1')
+
+
+def calcpearsonr():
+    plot = XYplot(t)
+    for gender in range(len(gender_CN)):
+        for catagory_c in range(len(catagory1)):
+            want2write = []
+            for count_protein in range(4, len(t_head)):
+                listAge = list(t.loc[(t['gender'] == gender_CN[gender]) & (t['catagory'] == catagory1[catagory_c]), t_head[3]])
+                listFeature = list(t.loc[(t['gender'] == gender_CN[gender]) & (t['catagory'] == catagory1[catagory_c]), t_head[count_protein]])
+                # print(listAge)
+                # print(listFeature)
+                # exit()
+                want2write.append(calc_corr(listAge, listFeature))
+            plot.WriteSheet(want2write, r'Pearsonr.xlsx', catagory1[catagory_c]+gender_CN[gender])
+    deleteSheet('Pearsonr.xlsx', 'Sheet1')
+
+
+calcpearsonr()
+
+# print(len(t_head))
